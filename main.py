15526cb764c2be11e6ffa5e58620ee7091725a44
Vladyslav Kotsiuba <92172262+vkotsiuba99@users.noreply.github.com>
2023-09-16 15:29:18 +0300

    basic setup

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from settings import *
 import moderngl as mgl
 import pygame as pg
 import sys
+from shader_program import ShaderProgram
+from scene import Scene
+from player import Player
 
 
 class VoxelEngine:
@@ -22,15 +25,29 @@ class VoxelEngine:
         self.delta_time = 0
         self.time = 0
 
+        pg.event.set_grab(True)
+        pg.mouse.set_visible(False)
+
         self.is_running = True
+        self.on_init()
+
+    def on_init(self):
+        self.player = Player(self)
+        self.shader_program = ShaderProgram(self)
+        self.scene = Scene(self)
 
     def update(self):
+        self.player.update()
+        self.shader_program.update()
+        self.scene.update()
+
         self.delta_time = self.clock.tick()
         self.time = pg.time.get_ticks() * 0.001
         pg.display.set_caption(f'{self.clock.get_fps():.0f}')
 
     def render(self):
         self.ctx.clear(color=BG_COLOR)
+        self.scene.render()
         pg.display.flip()
 
     def handle_events(self):
